19009a1845ad1583061e54fba3a6dfbbf6ee3239
Александр <shsashao811@gmail.com>
2026-04-06 18:12:06 +0700

    bureau features

--- src/app/modelling/features/bureau_features.py
+++ src/app/modelling/features/bureau_features.py
@@ -10,11 +10,19 @@ def generate_bureau_features(df):
     features = df[["SK_ID_CURR"]].copy()
 
     # 1. Максимальная сумма просрочки
+    features["MAX_OVERDUE"] = df.groupby("SK_ID_CURR")["AMT_CREDIT_SUM_OVERDUE"].max()
     # 2. Минимальная сумма просрочки
+    features["MAX_OVERDUE"] = df.groupby("SK_ID_CURR")["AMT_CREDIT_SUM_OVERDUE"].min()
     # 3. Какую долю суммы от открытого займа просрочил
+    total_credit = df.groupby("SK_ID_CURR")["AMT_CREDIT_SUM"].sum()
+    total_overdue = df.groupby("SK_ID_CURR")["AMT_CREDIT_SUM_OVERDUE"].sum()
+    features["OVERDUE_PROPORTION"] = total_overdue / total_credit
     # 4. Кол-во кредитов определенного типа
+
     # 5. Кол-во просрочек кредитов определенного типа
+
     # 6. Кол-во закрытых кредитов определенного типа
+    
     return features
 
 
